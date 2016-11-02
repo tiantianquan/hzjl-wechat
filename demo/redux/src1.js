9308f7b0ee7b1a011bbfc0c42e1f3cbf5707b7dd
tiantianquan <fenqiufen@gmail.com>
2016-11-03 00:27:16 +0800

    redux demo 多 reducer

--- demo/redux/src1.js
+++ demo/redux/src1.js
@@ -1,4 +1,5 @@
 var createStore = require('redux').createStore
+var combineReducers = require('redux').combineReducers
 
 /**
  * This is a reducer, a pure function with (state, action) => state signature.
@@ -23,13 +24,30 @@ function counter(state = 0, action) {
   }
 }
 
+function log(state = 'Init', action) {
+  switch (action.type) {
+    case 'A':
+      return 'A'
+    case 'B':
+      return 'B'
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  counterStore: counter,
+  logStore: log
+})
+
+
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(counter)
-  // You can use subscribe() to update the UI in response to state changes.
-  // Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
-  // However it can also be handy to persist the current state in the localStorage.
+let store = createStore(rootReducer)
 
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// However it can also be handy to persist the current state in the localStorage.
 store.subscribe(() => {
   console.log(store.getState())
 })
@@ -37,14 +55,20 @@ store.subscribe(() => {
 // The only way to mutate the internal state is to dispatch an action.
 // The actions can be serialized, logged or stored and later replayed.
 store.dispatch({
-    type: 'INCREMENT'
-  })
-  // 1
+  type: 'INCREMENT'
+})
+
 store.dispatch({
-    type: 'INCREMENT'
-  })
-  // 2
+  type: 'INCREMENT'
+})
+
 store.dispatch({
-    type: 'DECREMENT'
-  })
-  // 1
+  type: 'DECREMENT'
+})
+
+store.dispatch({
+  type: 'A'
+})
+store.dispatch({
+  type: 'B'
+})
