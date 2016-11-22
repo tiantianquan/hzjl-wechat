8cf107d91ab9ca7c81c3d5343dd5f5116d17ce54
TIANTIANQUAN-PC\tiantianquan <fenqiufen@gmail.com>
2016-11-22 19:10:56 +0800

    地图

--- demo/baiduMap/main.js
+++ demo/baiduMap/main.js
@@ -1,19 +1,30 @@
-﻿var map = new BMap.Map(
+﻿/**
+ * 地图对象
+ */
+var map = new BMap.Map(
   "allmap", {
     //最小缩放
     // minZoom: 7,
-     //最大缩放
+    //最大缩放
     // maxZoom: 10
   }
 )
-map.centerAndZoom('北京', 7)
-map.enableScrollWheelZoom(true)
+map.centerAndZoom('北京', 6)
+map.disableScrollWheelZoom()
+map.disableDragging()
+map.disableDoubleClickZoom()
+map.disablePinchToZoom()
 
 map.setMapStyle({
-  styleJson: mapStyle
-})
+    styleJson: mapStyle
+  })
 
 
+/**
+ * 格式化百度数据
+ * 
+ * @param {any} baiduData
+ */
 function getXYZ(baiduData) {
   this.name = baiduData.n
   var xyz = baiduData.g.split(',')
@@ -23,12 +34,9 @@ function getXYZ(baiduData) {
   this.z = yz[1]
 }
 
-// var city = {
-//   name: '天津市',
-//   x: 117.210813,
-//   y: 39.14393,
-//   z: 12,
-// }
+/**
+ * 城市类
+ */
 var City = function (baiduData, opt) {
   getXYZ.call(this, baiduData)
   for (var key in opt) {
@@ -38,8 +46,15 @@ var City = function (baiduData, opt) {
   }
   City.list.push(this)
 }
+
+/**
+ * 城市列表
+ */
 City.list = []
 
+/**
+ * 添加标记
+ */
 City.prototype.addMarker = function () {
   var point = new BMap.Point(this.x, this.y)
   var marker = new BMap.Marker(point)
@@ -59,85 +74,135 @@ City.prototype.addMarker = function () {
     border: null
   })
   marker.setLabel(label)
+    marker.addEventListener('click',function(){
+      alert(1)
+    })
   map.addOverlay(marker)
 }
 
 
-// var province = {
-//   name: '天津',
-//   x: 117.210813,
-//   y: 39.14393,
-//   z: 12,
-//   strokeWeight: 2,
-//   strokeColor: '#000000',
-//   fillColor: '#ffffff'
-// }
+/**
+ * 省市类
+ */
 var Province = function (baiduData, opt) {
-  getXYZ.call(this, baiduData)
-  for (var key in opt) {
-    if (opt.hasOwnProperty(key)) {
-      this[key] = opt[key];
+    getXYZ.call(this, baiduData)
+    for (var key in opt) {
+      if (opt.hasOwnProperty(key)) {
+        this[key] = opt[key];
+      }
     }
+    Province.list.push(this)
   }
-  Province.list.push(this)
-}
+  /**
+   * 省市列表
+   */
 Province.list = []
 
+/**
+ * 获取省市边界
+ */
 Province.prototype.getBoundary = function () {
   var boundary = new BMap.Boundary()
   var that = this
   boundary.get(this.name, function (rs) {
     var pointArray = []
     for (var i = 0; i < rs.boundaries.length; i++) {
-      var ply = new BMap.Polygon(rs.boundaries[i], {
-          strokeWeight: that.strokeWeight,
-          strokeColor: that.strokeColor,
-          fillColor: that.fillColor,
-          strokeOpacity: that.strokeOpacity,
-          fillOpacity: that.fillOpacity
-        }) //建立多边形覆盖物
+      var ply = that.drawMapBoundary(rs.boundaries[i])
       map.addOverlay(ply)
-      pointArray = pointArray.concat(ply.getPath());
+      that.registEvent(ply)
+        // pointArray = pointArray.concat(ply.getPath())
     }
   })
 }
+
+/**
+ * 填充省市
+ */
+Province.prototype.drawMapBoundary = function (boundary) {
+  var ply = new BMap.Polygon(boundary, {
+      strokeWeight: this.strokeWeight,
+      strokeColor: this.strokeColor,
+      fillColor: this.fillColor,
+      strokeOpacity: this.strokeOpacity,
+      fillOpacity: this.fillOpacity
+    }) //建立多边形覆盖物
+  return ply
+}
+
+/**
+ * 注册事件
+ */
+Province.prototype.registEvent = function (overlay) {
+  var that = this
+  overlay.addEventListener('click', function (e) {
+    console.log(e)
+    e.preventDefault()
+    return
+  })
+}
+
+/**
+ * 省市名称水印
+ */
 Province.prototype.addName = function () {
   var opts = {
     position: new BMap.Point(this.x, this.y), // 指定文本标注所在的地理位置
-    offset: new BMap.Size(-50, -50) //设置文本偏移量
+    offset: new BMap.Size(-15, -15) //设置文本偏移量
   }
   var label = new BMap.Label(this.name, opts) // 创建文本标注对象
   label.setStyle({
     color: "#fff",
-    fontSize: "50px",
+    fontSize: "15px",
     // height: "20px",
     // lineHeight: "2px",
-    fontFamily: "楷体",
+    fontFamily: "微软雅黑",
     backgroundColor: null,
     border: null,
-    opacity:0.5,
+    opacity: 0.5,
   })
+  this.registEvent(label)
   map.addOverlay(label)
 }
 
-for (var i = provinceData.length - 1; i >= 0; i--) {
-  var provinceBaidu = provinceData[i]
-  var province = new Province(provinceBaidu, {
-    strokeWeight: 0,
-    strokeColor: 'blue',
-    fillColor: provinceBaidu.fillColor,
-    fillOpacity: 0.3,
-    strokeOpacity: 0.2
-  })
-  province.getBoundary()
-  if (province.name !== '北京' && province.name !== '天津')
+//渲染-----------------------------------------------------------------------
+
+/**
+ * 渲染省
+ */
+function renderProvince() {
+  for (var i = provinceData.length - 1; i >= 0; i--) {
+    var provinceBaidu = provinceData[i]
+    var province = new Province(provinceBaidu, {
+      strokeWeight: null,
+      strokeColor: '#9e9a9a',
+      fillColor: provinceBaidu.fillColor,
+      fillOpacity: 0.3,
+      strokeOpacity: 0.1
+    })
+    province.getBoundary()
+
+    // if (province.name !== '北京' && province.name !== '天津') {
     province.addName()
+      // }
+  }
 }
 
-for (var i = 0; i < cityData.length; i++) {
-  var cityStr = cityData[i]
-  var city = new City(cityStr)
-  city.addMarker()
+/**
+ * 渲染市
+ */
+function renderCity() {
+  for (var i = 0; i < cityData.length; i++) {
+    var cityStr = cityData[i]
+    var city = new City(cityStr)
+    city.addMarker()
+  }
 }
 
+renderProvince()
+// renderCity()
 
+
+map.addEventListener('touchend', function (e) {
+  console.log(e)
+  e.domEvent.srcElement.click()
+})
